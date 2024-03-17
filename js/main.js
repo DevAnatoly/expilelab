41e532a6b5235899f466ff20ad393a799f9f34c9
Anatoly <home@dev.local>
2024-03-17 11:12:42 +0300

    header adaptive

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 Fancybox.bind("[data-fancybox]", {
     // Your custom options
   });
+
+// mobile menu
+ const mobileMenu = document.querySelector('.mobile-menu');
+ const menuList = document.querySelector('.menu__list');
+ const crossButton = document.querySelector('.close'); 
+
+  mobileMenu.addEventListener('click', function(){
+    menuList.classList.add('open');
+  });
+  crossButton.addEventListener('click', function(){
+    menuList.classList.remove('open');
+  });
